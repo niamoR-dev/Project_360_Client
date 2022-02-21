7b5c455e1635681b03d9f51982e0c61b3f4c88f2
DavAjz <david7720@hotmail.fr>
2022-02-21 11:42:01 +0100

    Suite Article Client

---
+++ src/app/core/web-services/items-customers.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, OnInit } from "@angular/core";
+import { IMIRequest, IMIResponse } from "@infor-up/m3-odin";
+import { MIService, UserService } from "@infor-up/m3-odin-angular";
+import { SohoMessageService } from "ids-enterprise-ng";
+import { Observable, of } from "rxjs";
+import { map, catchError } from 'rxjs/internal/operators';
+
+@Injectable({ providedIn: 'root' })
+export class ItemsCustomersService implements OnInit {
+
+   cuno: any;
+
+   constructor(protected miService: MIService, private userSevice: UserService, private messageService: SohoMessageService) {
+   }
+
+   ngOnInit() {
+   }
+
+
+   listeItemsCustomers() {
+
+      return this.listAllItemsCustomers().pipe(map((answer) => {                       // méthode qui permets d'envoyer la donnée vers le TS
+         if (answer.errorCode) {
+            throw Error(JSON.stringify(answer));
+         }
+         return answer.items;
+      }),
+         catchError((error) => {                                                    // gestion d'erreur selon la méthode que l'on a déclaréer en dessous
+            console.error('Erreur API list :', error);
+
+            this.handleError('Échec de l\'exécution de l\'API list', error);
+            return of(null);
+         })
+      );
+   }
+
+   //////////////////////////////////////////////////////////////////// Méthodes qui gère les erreurs ///////////////////////////////////////////////////////////////////////////////////
+
+   private handleError(message: string, error?: any) {
+      const buttons = [{ text: 'Ok', click: (e, modal) => { modal.close(); } }];
+      this.messageService.error()
+         .title('An error occured')
+         .message(message + '. More details might be available in the browser console.')
+         .buttons(buttons)
+         .open();
+   }
+
+
+   //////////////////////////////////////////////////////////////////// Méthodes qui appellent les API   ///////////////////////////////////////////////////////////////////////////////////
+
+
+   private listAllItemsCustomers(): Observable<IMIResponse> {
+
+      let inputFields: any = {                                                // ici on rentre les champs d'entrées obligatoires et optionnelles
+
+         CUNO: 'NDUCLI02'
+      }
+
+      const request: IMIRequest = {                                                // ici, on renseigne les champs de sorties que l'on veut afficher
+         program: 'OIS005MI',
+         transaction: 'List',
+         record: inputFields,
+         outputFields: ['CUNO', 'ITNO', 'ITDS'],
+      };
+      return this.miService.execute(request);
+   }
+}
--- src/app/tab-items-customers/tab-items-customers/tab-items-customers.component.ts
+++ src/app/tab-items-customers/tab-items-customers/tab-items-customers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CoreBase, IUserContext } from '@infor-up/m3-odin';
 import { MIService, UserService } from '@infor-up/m3-odin-angular';
+import { ItemsCustomersService } from 'src/app/core/web-services/items-customers.service';
 
 @Component({
    selector: 'app-tab-items-customers',
@@ -10,16 +11,85 @@ import { MIService, UserService } from '@infor-up/m3-odin-angular';
 export class TabItemsCustomersComponent extends CoreBase implements OnInit {
 
 
-   constructor(private miService: MIService, private userService: UserService) {
+   datagridOptions: SohoDataGridOptions = {};
+
+   listItemCustomer: any[]; // tableau pour enregistrer le retour d'API des articles du client
+
+   popn: any;
+   itds: any;
+   alum: any;
+   d2qt: any;
+   d3qt: any;
+   resp: any;
+   adcu: any;
+
+
+
+
+   constructor(private miService: MIService, private userService: UserService, private itemsCustomersService: ItemsCustomersService) {
       super('TabItemsCustomersComponent');
    }
 
    ngOnInit() {
+      this.itemsCustomersService.listeItemsCustomers().subscribe(data => {
 
+         this.listItemCustomer = data;
+         this.initGridItemCustomer();
+
+      });
+
+   }
+
+   private initGridItemCustomer() {                             // méthode qui permet d'afficher les données dans la GRID
+      const options: SohoDataGridOptions = {
+         selectable: 'single' as SohoDataGridSelectable,
+         disableRowDeactivation: true,
+         clickToSelect: true,
+         alternateRowShading: true,
+         cellNavigation: false,
+         idProperty: 'List',
+         paging: true,
+         pagesize: 10,
+         indeterminate: false,
+         columns: [
+            {
+               width: 50, id: 'selectionCheckbox', field: '', name: '', sortable: false,
+               resizable: false, align: 'center', formatter: Soho.Formatters.SelectionCheckbox, hidden: true
+            },
+            {
+               width: 'auto', id: 'col-cuno', field: 'CUNO', name: 'Client',
+               resizable: true, filterType: 'text', sortable: true
+            },
+            {
+               width: 'auto', id: 'col-itno', field: 'ITNO', name: 'Code article',
+               resizable: true, filterType: 'text', sortable: true
+            },
+            {
+               width: 'auto', id: 'col-itds', field: 'ITDS', name: 'Nom',
+               resizable: true, filterType: 'text', sortable: true
+            },
+
+         ],
+         dataset: this.listItemCustomer,
+         emptyMessage: {
+            title: 'Aucun article client à afficher',
+            icon: 'icon-empty-no-data'
+         }
+      };
+      this.datagridOptions = options;
    }
 
 
 
+   onSelectedLine(args: any[]) {                                        // méthode pour gérer quand on cique sur une ligne
+
+      const newCount = args.length;
+      const selected = args && newCount === 1 ? args[0].data : null;
+
+      //this.show = true;
+
+      this.itds = selected.ITDS;
+   }
 
 
 }
