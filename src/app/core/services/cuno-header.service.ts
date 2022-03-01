c9c9ed92c187cc7cdcfcc3b93d879ca049da5708
Romain <lemaire.romain37@gmail.com>
2022-03-01 10:03:47 +0100

    gitflow-feature-stash: essai-subject

--- src/app/core/services/cuno-header.service.ts
+++ src/app/core/services/cuno-header.service.ts
@@ -1,21 +1,27 @@
 
 import { Injectable } from '@angular/core';
 import { MIService, UserService } from '@infor-up/m3-odin-angular';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
    providedIn: 'root'
 })
 export class CunoHeaderService {
 
-   cuno: any = 15;
+   cuno: any;
    cunoSubject = new Subject<any>();
 
-   constructor(protected miService: MIService, private userSevice: UserService,) { }
+   constructor(protected miService: MIService, private userSevice: UserService,) {
+   }
 
 
    ngOnInit() {
 
+   }
+
+   cunoToSend(cuno: any) {
+
+      return this.cuno = cuno;
    }
 
 
