d03fbd943d254bb64d35885fca73b73ce38c8811
DavAjz <david7720@hotmail.fr>
2022-03-31 17:55:53 +0200

    api contrats

--- src/app/core/web-services/contracts.webservice.ts
+++ src/app/core/web-services/contracts.webservice.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { MIService } from '@infor-up/m3-odin-angular';
-import { SohoMessageService } from 'ids-enterprise-ng';
-import { of } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { IMIRequest, IMIResponse } from "@infor-up/m3-odin";
+import { MIService } from "@infor-up/m3-odin-angular";
+import { SohoMessageService } from "ids-enterprise-ng";
+import { Observable, of } from "rxjs";
+import { map, catchError } from 'rxjs/internal/operators';
 
-@Injectable({
-   providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ContractsWebService {
 
    //////////////////////////////////////////////////////////////////// Déclaration des variables ///////////////////////////////////////////////////////////////////////////////////
@@ -16,17 +16,55 @@ export class ContractsWebService {
    //////////////////////////////////////////////////////////////////// Constructeur d'appel des autres components/services ///////////////////////////////////////////////////////////////////////////////////
 
 
-   constructor(protected miService: MIService, private messageService: SohoMessageService) { }
+   constructor(protected miService: MIService, private messageService: SohoMessageService) {
+   }
 
 
    //////////////////////////////////////////////////////////////////// Méthode Init ///////////////////////////////////////////////////////////////////////////////////
 
-
-   recoveryCunoFromHeader(cuno: any) { // méthode qui récupère leCUNO du Header venant du component.ts Adresse
+   recoveryCunoFromHeader(cuno: any) { // méthode qui récupère leCUNO du Header venant du component.ts Item Customer
       return of(this.cunoHeader = cuno);
 
    }
 
+   listeContracts() {
+
+
+      return this.listAllContracts().pipe(map((answer) => {
+         console.log('prout', answer)                       // méthode qui permets d'envoyer la donnée vers le TS
+         if (answer.errorCode) {
+            throw Error(JSON.stringify(answer));
+         }
+         return answer.items;
+      }),
+         catchError((error) => {                                                    // gestion d'erreur selon la méthode que l'on a déclaréer en dessous
+            console.error('Erreur API list :', error);
+
+            this.handleError('Échec de l\'exécution de l\'API list', error);
+            return of(null);
+         })
+      );
+   }
+
+
+   //////////////////////////////////////////////////////////////////// Méthode Détails Grid ///////////////////////////////////////////////////////////////////////////////////
+
+   // listDetailContracts() {
+
+   //    return this.listALLDetailContracts().pipe(map((answer) => {                       // méthode qui permets d'envoyer la donnée vers le TS
+   //       if (answer.errorCode) {
+   //          throw Error(JSON.stringify(answer));
+   //       }
+   //       return answer.items;
+   //    }),
+   //       catchError((error) => {                                                    // gestion d'erreur selon la méthode que l'on a déclaréer en dessous
+   //          console.error('Erreur API list :', error);
+
+   //          this.handleError('Échec de l\'exécution de l\'API list', error);
+   //          return of(null);
+   //       })
+   //    );
+   // }
 
    //////////////////////////////////////////////////////////////////// Méthodes qui gère les erreurs ///////////////////////////////////////////////////////////////////////////////////
 
@@ -41,4 +79,210 @@ export class ContractsWebService {
 
 
    //////////////////////////////////////////////////////////////////// Méthodes qui appellent les API   ///////////////////////////////////////////////////////////////////////////////////
+
+
+   private listAllContracts(): Observable<IMIResponse> {
+
+      let inputFields: any = {                                                // ici on rentre les champs d'entrées obligatoires et optionnelles
+
+         UYCUNO: 'NDUCLI03',
+         UYAGNO: '',
+         UYSTDT: ''
+
+      }
+
+      const request: IMIRequest = {                                                // ici, on renseigne les champs de sorties que l'on veut afficher
+         program: 'CMS100MI',
+         transaction: 'LstContrat',
+         record: inputFields,
+         outputFields: ['UYCUNO', 'UYAGNO', 'UYSTDT', 'UYAGTP', 'UYTX40', 'UYAGST'],
+      };
+      console.log('ca marche pas', request)
+      return this.miService.execute(request);
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Injectable } from '@angular/core';
+// import { MIService } from '@infor-up/m3-odin-angular';
+// import { SohoMessageService } from 'ids-enterprise-ng';
+// import { of } from 'rxjs';
+
+// @Injectable({
+//    providedIn: 'root'
+// })
+// export class ContractsWebService {
+//    listDetailContracts() {
+//       throw new Error('Method not implemented.');
+//    }
+
+//    //////////////////////////////////////////////////////////////////// Déclaration des variables ///////////////////////////////////////////////////////////////////////////////////
+
+//    cunoHeader: any;
+
+
+//    //////////////////////////////////////////////////////////////////// Constructeur d'appel des autres components/services ///////////////////////////////////////////////////////////////////////////////////
+
+
+//    constructor(protected miService: MIService, private messageService: SohoMessageService) { }
+
+
+//    //////////////////////////////////////////////////////////////////// Méthode Init ///////////////////////////////////////////////////////////////////////////////////
+
+
+//    recoveryCunoFromHeader(cuno: any) { // méthode qui récupère leCUNO du Header venant du component.ts Adresse
+//       return of(this.cunoHeader = cuno);
+
+//    }
+
+
+//    //////////////////////////////////////////////////////////////////// Méthodes qui gère les erreurs ///////////////////////////////////////////////////////////////////////////////////
+
+//    private handleError(message: string, error?: any) {
+//       const buttons = [{ text: 'Ok', click: (e, modal) => { modal.close(); } }];
+//       this.messageService.error()
+//          .title('An error occured')
+//          .message(message + '. More details might be available in the browser console.')
+//          .buttons(buttons)
+//          .open();
+//    }
+
+
+//    //////////////////////////////////////////////////////////////////// Méthodes qui appellent les API   ///////////////////////////////////////////////////////////////////////////////////
+// }
--- src/app/tab-contracts/tab-contracts/tab-contracts.component.ts
+++ src/app/tab-contracts/tab-contracts/tab-contracts.component.ts
@@ -1,63 +1,322 @@
 import { Component, OnInit } from '@angular/core';
-import { CoreBase } from '@infor-up/m3-odin';
-import { Subscription } from 'rxjs';
-import { CunoHeaderService } from 'src/app/core/services/cuno-header-service/cuno-header.service';
+import { CoreBase, IUserContext } from '@infor-up/m3-odin';
+import { MIService, UserService } from '@infor-up/m3-odin-angular';
 import { ContractsWebService } from 'src/app/core/web-services/contracts.webservice';
 
 @Component({
-   selector: 'app-tab-contrats',
+   selector: 'app-tab-contracts',
    templateUrl: './tab-contracts.component.html',
    styleUrls: ['./tab-contracts.component.css']
 })
 export class TabContractsComponent extends CoreBase implements OnInit {
 
 
-   //////////////////////////////////////////////////////////////////// Déclaration des variables ///////////////////////////////////////////////////////////////////////////////////
+   datagridOptions: SohoDataGridOptions = {};
 
-   cunoHeader$: any;
-   cunoSubscription: Subscription;
-   listContractsClient: any[]; // tableau pour enregistrer le retour d'API des adresses d'un client
+   listContracts: any[]; // tableau pour enregistrer le retour d'API des articles du client
+
+   cuno: any;
+   agno: any;
+   stdt: any;
+   agtp: any;
+   tx40: any;
+   agst: any;
 
 
-   //////////////////////////////////////////////////////////////////// Constructeur d'appel des autres components ///////////////////////////////////////////////////////////////////////////////////
 
 
-   constructor(private contractsWebService: ContractsWebService, private cunoHeaderService: CunoHeaderService) {
+   listDetailContracts: any;
+
+
+
+
+   constructor(private miService: MIService, private userService: UserService, private contractsWebService: ContractsWebService) {
       super('TabContractsComponent');
    }
 
-   //////////////////////////////////////////////////////////////////// Méthode Init ///////////////////////////////////////////////////////////////////////////////////
-
-
-
    ngOnInit() {
-      this.cunoHeaderMethod(); // lancement de la méthode de récupération du CUNO
 
-      this.sendCunoToContractsWebService(); // lancement de la méthode de récupération du CUNO
+      this.recoveryDataFromApi();
 
-      //this.recoveryDataFromAPI(); // lancement de la méthode de récupération des donnés qui lance aussi l'initialisation de la Grid
+   }
+   private recoveryDataFromApi() {
+      this.contractsWebService.listeContracts().subscribe(data => {
+         console.log('ngoninit', data)
+         this.listContracts = data;
+         this.initGridContracts();
+      });
 
    }
 
+   private initGridContracts() {                             // méthode qui permet d'afficher les données dans la GRID
+      const options: SohoDataGridOptions = {
+         selectable: 'single' as SohoDataGridSelectable,
+         disableRowDeactivation: true,
+         clickToSelect: true,
+         alternateRowShading: true,
+         cellNavigation: false,
+         idProperty: 'List',
+         paging: true,
+         pagesize: 10,
+         indeterminate: false,
+         columns: [
+            {
+               width: 50, id: 'selectionCheckbox', field: '', name: '', sortable: false,
+               resizable: false, align: 'center', formatter: Soho.Formatters.SelectionCheckbox, hidden: true
+            },
+            {
+               width: 'auto', id: 'col-cuno', field: 'UYCUNO', name: 'Client',
+               resizable: true, filterType: 'text', sortable: true
+            },
+            {
+               width: 'auto', id: 'col-agno', field: 'UYAGNO', name: 'Cde Ouv',
+               resizable: true, filterType: 'text', sortable: true
+            },
+            {
+               width: 'auto', id: 'col-stdt', field: 'UYSTDT', name: 'Dt Déb',
+               resizable: true, filterType: 'text', sortable: true
+            },
+            {
+               width: 'auto', id: 'col-agtp', field: 'UYAGTP', name: 'Typ',
+               resizable: true, filterType: 'text', sortable: true
+            },
+            {
+               width: 'auto', id: 'col-tx40', field: 'UYTX40', name: 'Désignation',
+               resizable: true, filterType: 'text', sortable: true
+            },
+            {
+               width: 'auto', id: 'col-agst', field: 'UYAGST', name: 'Stt',
+               resizable: true, filterType: 'text', sortable: true
+            },
 
-
-   //////////////////////////////////////////////////////////////////// Méthodes qui gère l'affichage Grid ///////////////////////////////////////////////////////////////////////////////////
-
-
-   cunoHeaderMethod() {    // méthode observable pour récupérer la CUNO de la dropdown du header
-      this.cunoSubscription = this.cunoHeaderService.cunoSubject.subscribe(
-         (data: any) => {
-            this.cunoHeader$ = data;
+         ],
+         dataset: this.listContracts,
+         emptyMessage: {
+            title: 'Aucun article client à afficher',
+            icon: 'icon-empty-no-data'
          }
-      );
-      this.cunoHeaderService.subjectMethod();
+      };
+      this.datagridOptions = options;
    }
 
 
 
-   sendCunoToContractsWebService() {       // méthode obesevable pour envoyer la CUNO de le webService de Adresse
-      this.contractsWebService.recoveryCunoFromHeader(this.cunoHeader$).subscribe();
-   }
+   // onSelectedLine(args: any[]) {                                        // méthode pour gérer quand on cique sur une ligne
+
+   //    const newCount = args.length;
+   //    const selected = args && newCount === 1 ? args[0].data : null;
+   //    this.InitDetailContacts();
+   //    //this.show = true;
+
+   //    this.itds = selected.ORITDS;
+   // }
+
+   // private InitDetailItemCustomerList() { // API OIS005MI
+
+   //    this.itemsContractsWebService.listDetailContracts().subscribe(data => {
+   //       this.listDetailContracts = data;
+
+   //       this.cuno = this.listDetailContracts[0].CUNO;
+   //       this.agno = this.listDetailContracts[0].AGNO;
+   //       this.stdt = this.listDetailContracts[0].STDT;
+   //       this.agtp = this.listDetailContracts[0].AGTP;
+   //       this.tx40 = this.listDetailContracts[0].TX40;
+   //       this.agst = this.listDetailContracts[0].AGST;
+
+   //       console.log(" GetBasicData  ", this.listDetailContracts)  // la virgule dans le console log permets de lire à 'intérieur de l'objet
+
+   //    });
+   // }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { CoreBase } from '@infor-up/m3-odin';
+// import { Subscription } from 'rxjs';
+// import { CunoHeaderService } from 'src/app/core/services/cuno-header-service/cuno-header.service';
+// import { ContractsWebService } from 'src/app/core/web-services/contracts.webservice';
+
+// @Component({
+//    selector: 'app-tab-contrats',
+//    templateUrl: './tab-contracts.component.html',
+//    styleUrls: ['./tab-contracts.component.css']
+// })
+// export class TabContractsComponent extends CoreBase implements OnInit {
+
+
+//    //////////////////////////////////////////////////////////////////// Déclaration des variables ///////////////////////////////////////////////////////////////////////////////////
+
+//    cunoHeader$: any;
+//    cunoSubscription: Subscription;
+//    listContractsClient: any[]; // tableau pour enregistrer le retour d'API des adresses d'un client
+
+
+//    //////////////////////////////////////////////////////////////////// Constructeur d'appel des autres components ///////////////////////////////////////////////////////////////////////////////////
+
+
+//    constructor(private contractsWebService: ContractsWebService, private cunoHeaderService: CunoHeaderService) {
+//       super('TabContractsComponent');
+//    }
+
+//    //////////////////////////////////////////////////////////////////// Méthode Init ///////////////////////////////////////////////////////////////////////////////////
+
+
+
+//    ngOnInit() {
+//       this.cunoHeaderMethod(); // lancement de la méthode de récupération du CUNO
+
+//       this.sendCunoToContractsWebService(); // lancement de la méthode de récupération du CUNO
+
+//       //this.recoveryDataFromAPI(); // lancement de la méthode de récupération des donnés qui lance aussi l'initialisation de la Grid
+
+//    }
+
+
+
+//    //////////////////////////////////////////////////////////////////// Méthodes qui gère l'affichage Grid ///////////////////////////////////////////////////////////////////////////////////
+
+
+//    cunoHeaderMethod() {    // méthode observable pour récupérer la CUNO de la dropdown du header
+//       this.cunoSubscription = this.cunoHeaderService.cunoSubject.subscribe(
+//          (data: any) => {
+//             this.cunoHeader$ = data;
+//          }
+//       );
+//       this.cunoHeaderService.subjectMethod();
+//    }
+
+
+
+//    sendCunoToContractsWebService() {       // méthode obesevable pour envoyer la CUNO de le webService de Adresse
+//       this.contractsWebService.recoveryCunoFromHeader(this.cunoHeader$).subscribe();
+//    }
 
 
 
@@ -74,4 +333,5 @@ export class TabContractsComponent extends CoreBase implements OnInit {
 
 
 
-}
+
+//}
